a82cd82985d7cb50d45e982c5bda3288a8201336
suan7000 <suan7000@126.com>
2018-10-13 19:01:37 +0800

    Revert "Initial commit"
    This reverts commit 6d9efcb6b46d453995da04fe92c118d6448e885e.

--- Example12.py
+++
@@ -1,8 +0,0 @@
-age = raw_input("How old are you?")
-print "How tall are you?",
-height = raw_input()
-print "How much dou you weight?"
-weight = raw_input()
-
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
-print "So, you're %s old, %s tall and %s heavy." % (age, height, weight)
